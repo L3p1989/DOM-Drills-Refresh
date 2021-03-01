a6406118a266316df6c8a93a6ed8220751374473
L3p1989 <phogan110@live.com>
2021-03-01 16:07:03 -0700

    wrapped code in DOMContentLoaded event listener. created h1. created h1Text. added h1Text to h1. added h1 to div. added div to body.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-let div = document.createElement('div').className = 'header-container';
+//Wait for DOM to load
+document.addEventListener("DOMContentLoaded", () => {
+    //Create div
+    let div = document.createElement('div');
+    //Give div className "header-container"
+    div.className = "header-container";
+    //Create h1
+    let h1 = document.createElement('h1');
+    //Create text "This is an h1"
+    let h1Text = document.createTextNode('This is an h1');
+    //Add h1Text to h1
+    h1.appendChild(h1Text);
+    //Add h1 inside div.header-container
+    div.appendChild(h1);
+    //Grab body element
+    document.body.appendChild(div)
+})
+
