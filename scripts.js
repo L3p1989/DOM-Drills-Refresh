f33da30b83e56da971a12793bc1496b7ea34396c
L3p1989 <phogan110@live.com>
2021-03-01 16:29:21 -0700

    followed same steps for h3-h6

--- scripts.js
+++ scripts.js
@@ -22,5 +22,36 @@ document.addEventListener("DOMContentLoaded", () => {
     h2.appendChild(h2Text);
     //Add `h2` to `div`
     div.appendChild(h2);
+    //Create h3
+    let h3 = document.createElement('h3');
+    //Create text
+    let h3Text = document.createTextNode('This is an h3');
+    //Add `h3Text to `h3`
+    h3.appendChild(h3Text);
+    //Add `h3` to `div`
+    div.appendChild(h3);
+    //Create h4
+    let h4 = document.createElement('h4');
+    //Create text
+    let h4Text = document.createTextNode('This is an h4');
+    //Add `h4Text to `h4`
+    h4.appendChild(h4Text);
+    //Add `h4` to `div`
+    div.appendChild(h4);
+    //Create h5
+    let h5 = document.createElement('h5');
+    //Create text
+    let h5Text = document.createTextNode('This is an h5');
+    //Add `h5Text to `h5`
+    h5.appendChild(h5Text);
+    //Add `h5` to `div`
+    div.appendChild(h5);
+    //Create h6
+    let h6 = document.createElement('h6');
+    //Create text
+    let h6Text = document.createTextNode('This is an h6');
+    //Add `h6Text to `h6`
+    h6.appendChild(h6Text);
+    //Add `h6` to `div`
+    div.appendChild(h6);
 });
-
