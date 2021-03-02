1a68793ecb82c44627edc37b2b8269d296976695
L3p1989 <phogan110@live.com>
2021-03-01 19:27:49 -0700

    created `headerClick` function that uses `randomColor` to change text color when the header tags inside of .header-container are dblclicked. created dblclick event listener for `divHeader` that uses `headerClick`

--- scripts.js
+++ scripts.js
@@ -76,4 +76,11 @@ document.addEventListener("DOMContentLoaded", () => {
     };
     //Grab .header-container
     let divHeader = document.getElementsByClassName('header-container');
+    //headerClick function that uses `randomColor` to change header text color on dblclick
+    let headerClick = (e) => {
+        //if not parent element change color of text otherwise text color remains the same
+        e.target !== e.currentTarget ? e.target.style.color = randomColor() : e.target.style.color = e.target.style.color;
+    };
+    //Create dblclick event listener for `divHeader`
+    divHeader[0].addEventListener('dblclick', headerClick, false);
 });
